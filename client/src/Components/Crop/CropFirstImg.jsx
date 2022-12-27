e7b8a5eb8e1393955c40289d921ce93535610c42
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2022-12-27 17:13:00 +0200

    post req (first img)

--- client/src/Components/Crop/CropFirstImg.jsx
+++ client/src/Components/Crop/CropFirstImg.jsx
@@ -5,20 +5,39 @@ import { FileContext } from '../contexts/fileContext'
 import axios from '../../Global/API/axios'
 
 const CropFirstImg = () => {
+    const [originalURL, setOriginalURL] = useState()
+    const [phaseURL, setphaseURL] = useState()
+    const [magnitudeURL, setMagnitudeURL] = useState()
     const inputFile = useRef(null);
-    const { firstCrop, setFirstCrop, firstFile, setFirstFile,selectOriginalFirst , setSelectOriginalFirst} = useContext(FileContext);
+    const { 
+        firstCrop,
+        setFirstCrop,
+        firstFile,
+        setFirstFile,
+        selectOriginalFirst,
+        setSelectOriginalFirst
+        } = useContext(FileContext);
 
     const handleFileFirstUpload = (e) => {
         setFirstFile(URL.createObjectURL(e.target.files[0]));
         const formData = new FormData();
-        formData.append("file1", e.target.files[0])
-        // axios.post('',
-        //     formData
-        // ).then((response) => {
-        //     console.log(response)
-        // }).catch((err) => {
-        //     console.log(err)
-        // })
+        formData.append("Img", e.target.files[0])
+        axios.post('/img',
+            formData
+        ).then((response) => {
+            setOriginalURL(response.data.img_url)
+            axios.get(`/combImg?imgId=${response.data.imgId}`
+            ).then((response) => {
+                setMagnitudeURL(response.data.mag_img_url)
+                setphaseURL(response.data.phase_img_url)
+                console.log(response)
+            }).catch((err) => {
+                console.log(err)
+            })
+            console.log(response)
+        }).catch((err) => {
+            console.log(err)
+        })
     }
     console.log(firstFile);
     console.log(firstCrop);
@@ -28,6 +47,7 @@ const CropFirstImg = () => {
     };
 
     const handleImgDelete = () => {
+        setOriginalURL(null)
         setFirstFile(undefined)
         inputFile.current.value = null
     }
@@ -42,7 +62,7 @@ const CropFirstImg = () => {
         <>
             <div>
                 <button onClick={handleSelectClick}>
-                    crop image 
+                    crop image 1
                 </button>
                 <button onClick={handleButtonClick}>
                     Upload image 1
@@ -58,7 +78,7 @@ const CropFirstImg = () => {
                     onChange={handleFileFirstUpload}
                 />
                 {selectOriginalFirst === true ? (
-                        <ReactCrop crop={firstCrop} onChange={(c,per) => setFirstCrop(per)}
+                    <ReactCrop crop={firstCrop} onChange={(c, per) => setFirstCrop(per)}
                         onComplete={(px, percent) => {
                             // axios.post('',
                             //     formData
@@ -68,12 +88,12 @@ const CropFirstImg = () => {
                             //     console.log(err)
                             // })
                         }}>
-                        <img style={{ width: firstFile !== undefined ? "20rem" : null, height: firstFile !== undefined ? "20rem" : null }} src={firstFile} />
+                        <img style={{ width: originalURL !== null ? "20rem" : "0", height: originalURL !== null? "20rem" : "0" }} src={originalURL} />
                     </ReactCrop>
-                ) : 
-                <img style={{ width: firstFile !== undefined ? "20rem" : null, height: firstFile !== undefined ? "20rem" : null }} src={firstFile} />
+                ) :
+                    <img style={{ width: firstFile !== undefined ? "20rem" : "0", height: firstFile !== undefined ? "20rem" : "0" }} src={originalURL} />
                 }
-                
+
             </div>
         </>
     )
