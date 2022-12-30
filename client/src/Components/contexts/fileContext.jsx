5f4a253291e8ae1be946aa8b49192de2cbcfbf68
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2022-12-30 02:33:46 +0200

    crop states

--- client/src/Components/contexts/fileContext.jsx
+++ client/src/Components/contexts/fileContext.jsx
@@ -3,84 +3,120 @@ const FileContext = createContext();
 
 const FileContextProvider = ({ children }) => {
     // original first
-    const [firstFile,setFirstFile] = useState();
+    const [firstFile, setFirstFile] = useState();
     const [originalFirstURL, setOriginalFirstURL] = useState()
     const [originalFirstImgId, setOriginalFirstImgId] = useState()
-    const [firstCrop, setFirstCrop] = useState({
-        unit: '%', // Can be 'px' or '%'
-        x: 0,
-        y: 0,
-        width: 0,
-        height: 0
-    })
+    const [firstCrop, setFirstCrop] = useState(
+        {
+            unit: '%', // Can be 'px' or '%'
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0
+        }
+    )
     // original second
-    const [secondFile,setSecondFile] = useState();
+    const [secondFile, setSecondFile] = useState();
     const [originalSecondURL, setOriginalSecondURL] = useState()
     const [originalSecondImgId, setOriginalSecondImgId] = useState()
-    const [secondCrop, setSecondCrop] = useState({
-        unit: '%', // Can be 'px' or '%'
-        x: 0,
-        y: 0,
-        width: 0,
-        height: 0
-    })
+    const [secondCrop, setSecondCrop] = useState(
+        {
+            unit: '%', // Can be 'px' or '%'
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0
+        }
+    )
     // mag + phase first
     const [magnitudeFirstURL, setMagnitudeFirstURL] = useState()
     const [phaseFirstURL, setphaseFirstURL] = useState()
-    const [magFirstCrop, setMagFirstCrop] = useState()
-    const [phaseFirstCrop, setPhaseFirstCrop] = useState()
+    const [magFirstCrop, setMagFirstCrop] = useState(
+        {
+            unit: '%', // Can be 'px' or '%'
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0
+        }
+    )
+    const [phaseFirstCrop, setPhaseFirstCrop] = useState(
+        {
+            unit: '%', // Can be 'px' or '%'
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0
+        }
+    )
     // mag + phase second
     const [magnitudeSecondURL, setMagnitudeSecondURL] = useState()
     const [phaseSecondURL, setphaseSecondURL] = useState()
-    const [magSecondCrop, setMagSecondCrop] = useState()
-    const [phaseSecondCrop, setPhaseSecondCrop] = useState()
+    const [magSecondCrop, setMagSecondCrop] = useState(
+        {
+            unit: '%', // Can be 'px' or '%'
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0
+        }
+    )
+    const [phaseSecondCrop, setPhaseSecondCrop] = useState(
+        {
+            unit: '%', // Can be 'px' or '%'
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0
+        }
+    )
     // global 
     const [mo, setMo] = useState(true)
-    const [checkMode , setCheckMode] = useState(false);
-	
-	return (
-		<FileContext.Provider 
-        value={{
-            firstCrop,
-            setFirstCrop,
-            firstFile,
-            setFirstFile,
-            secondCrop,
-            setSecondCrop,
-            secondFile,
-            setSecondFile,
-            originalFirstURL,
-            setOriginalFirstURL,
-            phaseFirstURL,
-            setphaseFirstURL,
-            magnitudeFirstURL,
-            setMagnitudeFirstURL,
-            originalSecondURL,
-            setOriginalSecondURL,
-            phaseSecondURL,
-            setphaseSecondURL,
-            magnitudeSecondURL,
-            setMagnitudeSecondURL,
-            checkMode,
-            setCheckMode,
-            magFirstCrop,
-            setMagFirstCrop,
-            phaseFirstCrop,
-            setPhaseFirstCrop,
-            magSecondCrop,
-            setMagSecondCrop,
-            phaseSecondCrop,
-            setPhaseSecondCrop,
-            mo,
-            setMo,
-            originalFirstImgId,
-            setOriginalFirstImgId,
-            originalSecondImgId,
-            setOriginalSecondImgId
+    const [checkMode, setCheckMode] = useState(false);
+
+    return (
+        <FileContext.Provider
+            value={{
+                firstCrop,
+                setFirstCrop,
+                firstFile,
+                setFirstFile,
+                secondCrop,
+                setSecondCrop,
+                secondFile,
+                setSecondFile,
+                originalFirstURL,
+                setOriginalFirstURL,
+                phaseFirstURL,
+                setphaseFirstURL,
+                magnitudeFirstURL,
+                setMagnitudeFirstURL,
+                originalSecondURL,
+                setOriginalSecondURL,
+                phaseSecondURL,
+                setphaseSecondURL,
+                magnitudeSecondURL,
+                setMagnitudeSecondURL,
+                checkMode,
+                setCheckMode,
+                magFirstCrop,
+                setMagFirstCrop,
+                phaseFirstCrop,
+                setPhaseFirstCrop,
+                magSecondCrop,
+                setMagSecondCrop,
+                phaseSecondCrop,
+                setPhaseSecondCrop,
+                mo,
+                setMo,
+                originalFirstImgId,
+                setOriginalFirstImgId,
+                originalSecondImgId,
+                setOriginalSecondImgId
             }}>
-			{children}
-		</FileContext.Provider>
-	);
+            {children}
+        </FileContext.Provider>
+    );
 };
 
 export { FileContext, FileContextProvider };
